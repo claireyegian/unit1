bb669a8a9ff42bd53e617aef43703d737e3fbbcc
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-08-31 14:53:57 -0400

    Updated from Brython Server: 8/31/2017 2:53:55 PM

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,1 +1,7 @@
+#Claire Yegian
+#8/31/17
+#tipCalculator.py - calculates tips
 
+price = int(input('Price of meal (in dollars): '))
+tip = int(input('% to tip: '))
+print('You should tip', price*(tip/100))
