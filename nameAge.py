598a9337f46ffa0181cf34cf7416627a994eaf05
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-08-31 14:50:16 -0400

    Updated from Brython Server: 8/31/2017 2:50:13 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,9 @@
+#Claire Yegian
+#8/31/17
+#nameAge.py - characters in a name and age next year
 
+name = input('Enter your first and last name: ')
+name1, name2 = name.split()
+print('Your first name has', len(name1), 'letters, and your last name has', len(name2), 'letters.')
+age = int(input('Enter your age: '))
+print('Next year you will be', age+1, 'years old')
