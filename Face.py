d99d8307c1d75ae0d5348c627b56a249a33790e6
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-08-31 14:33:47 -0400

    Updated from Brython Server: 8/31/2017 2:33:45 PM

--- Face.py
+++ Face.py
@@ -1,1 +1,10 @@
+#Claire Yegian
+#8/31/17
+#Face.py - programming practice face drawing
 
+print('    xxxxx    ')
+print('  x      x   ')
+print('((   o o   ))')
+print('  |   v   |  ')
+print('  |  ===  |  ')
+print('   _______   ')
