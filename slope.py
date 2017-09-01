1834970a1580f5cf3ed5a33923f5dd6fd012926d
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-01 14:43:01 -0400

    Updated from Brython Server: 9/1/2017 2:43:00 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,12 @@
+#Claire Yegian
+#9/1/17
+#slope.py - calculates slope of a line
 
+x1 = float(input('x1 = '))
+x2 = float(input('x2 = '))
+y1 = float(input('y1 = '))
+y2 = float(input('y2 = '))
+slope = float((y1-y2)/(x1-x2))
+intersection = float(y1-(slope*x1))
+print('The slope is', slope)
+print('The equation of the line is Y =', slope, 'X +', intersection)
