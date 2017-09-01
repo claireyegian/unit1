eb9c9dfca417268701f6069e6b2681491960fa19
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-01 14:53:33 -0400

    Updated from Brython Server: 9/1/2017 2:53:32 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,8 @@
+#Claire Yegian
+#9/1/17
+#stringAnalysis.py - reports words and characters in a sentence, finds occurences of entered words and characters
 
+sentence = input('Enter your sentence: ')
+spaces = int(sentence.count( ))
+characters = int(len(sentence))
+print('Your sentence has', spaces+1, 'words and', characters, 'characters and', characters-spaces, 'letters.')
