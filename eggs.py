cfa4dbb6ae873c5e443834ed36230076ea025056
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-06 13:54:32 -0400

    Updated from Brython Server: 9/6/2017 1:54:33 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,7 @@
+#Claire Yegian
+#9/6/17
+#eggs.py - will the eggs fit in the cartons?
 
+eggs = int(input('Input number of eggs: '))
+cartons = int(input('Input number of cartons: '))
+print(((eggs/12)<= cartons))
