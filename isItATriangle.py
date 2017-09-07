a456b2bb78be9142b8e866762a723cbdba9bf596
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-07 14:44:17 -0400

    Update isItATriangle.py

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,11 @@
+#Claire Yegian
+#9/7/17
+#isItATriangle.py - find if entered values can be the sides of a triangle
 
+side1 = float(input('Enter the length of the first side: '))
+side2 = float(input('Enter the length of the second side: '))
+side3 = float(input('Enter the length of the third side: '))
+long = float(max(side1,side2,side3))
+short = float(min(side1,side2,side3))
+mid = float(side1+side2+side3-long-short)
+print((mid+short)>long)
