5755dcb30662ba1323f18d52db7853212d227c5c
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-07 14:29:47 -0400

    Update change.py

--- change.py
+++ change.py
@@ -2,5 +2,9 @@
 #9/6/17
 #change.py - finds change in given amount of money
 
-money = int(input('Input a number of cents: ')
-quarters = (
+money = int(input('Input a number of cents: '))
+quarters = (money//25)
+dimes = ((money-(quarters*25))//10)
+nickels = ((money-(quarters*25+dimes*10))//5)
+pennies = ((money-(quarters*25+dimes*10+nickels*5))//1)
+print('You have', quarters, 'quarters,', dimes, 'dimes,', nickels, 'nickels, and', pennies, 'pennies.')
