b1600823c241416e5b4868bffe77c9fc0dc4f022
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-11 14:20:11 -0400

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,13 @@
+#Claire Yegian
+#9/11/17
+#quiz1.py - prints name, sum and product of two numbers, lucky number
 
+from random import randint
+
+name = input('Enter your name: ')
+print(name)
+num1 = float(input('Enter a number: '))
+num2 = float(input('Enter another number: '))
+print('The sum is', num1+num2)
+print('The product is', num1*num2)
+print('Your lucky number is', randint(9,9))
