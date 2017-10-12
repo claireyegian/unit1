d40599b163c8d52b744126b2d1ca4cb5f9288713
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-12 14:35:44 -0400

    Updated from Brython Server: 10/12/2017 2:35:44 PM

--- binary.py
+++ binary.py
@@ -5,8 +5,12 @@
 binary = int(input('Enter an eight-digit binary number: '))
 
 one = (binary%10)
-two = ((binary//100)%10)*2
-three = ((binary//1000)%10)*4
-four = ((binary/10000)%10)*8
+two = ((binary//10)%10)*2
+three = ((binary//100)%10)*4
+four = ((binary//1000)%10)*8
+five = ((binary//10000)%10)*16
+six = ((binary//100000)%10)*32
+seven = ((binary//1000000)%10)*64
+eight = ((binary//10000000)%10)*128
 
-print(one+two+three+four)
+print(one+two+three+four+five+six+seven+eight)
