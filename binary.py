030992a96f5b76a21e2e6487d5a5feed4dee5747
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-12 14:29:06 -0400

    Updated from Brython Server: 10/12/2017 2:29:06 PM

--- binary.py
+++ binary.py
@@ -1,1 +1,11 @@
+#Claire Yegian
+#10/12/17
+#binary.py - converts 8 digit binary to base 10
 
+binary = int(input('Enter an eight-digit binary number: '))
+
+one = (binary%10)
+two = ((binary//100)%10)*2
+three = ((binary//1000)%10)*4
+
+print(one+two+three)
